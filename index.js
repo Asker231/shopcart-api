528aacfe7883d33522a5590a6460b7dc31631214
Asker <kotsevasker0@gmail.com>
2024-03-19 18:10:32 +0300

    create api

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+app.use(express.json())
+const PORT = 3002
+
+app.get('/',(req,res)=>{
+    try {
+        res.status(200)
+        res.send("</h1>Hello cartshopAPI</h1>")
+    } catch (error) {
+        console.log(error);
+    }
+    res.end()
+})
+app.listen(PORT,(err)=>{
+    if(!err)console.log(`server started:http://localhost:${PORT}/ `);
+})
